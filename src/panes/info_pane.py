f587ca02b84b1f107d303cf559a4de3de7a4881a
Mharis13 <mario12ban@gmail.com>
2026-01-26 15:12:46 +0100

    Add Selector enviroments

--- src/panes/info_pane.py
+++ src/panes/info_pane.py
@@ -8,10 +8,28 @@ from widgets.environment_selector import EnvrionmentSelector
 
 
 class InformationPane(Widget):
+    DEFAULT_CSS = """
+    InformationPane {
+            border: #2a4f2c;
+            height: 25;
+            }
+    InformationPane > Horizontal > Static {
+            width: 11;
+            border: solid red;
+            align:center top;
+            }
+    .logo {
+            max-width:19;
+            height: 9;
+            border: red;
+            }
+    """
+
     @override
     def compose(self) -> ComposeResult:
         with Horizontal():
             yield Static("Test")
+            yield EnvrionmentSelector()
             yield EnvrionmentSelector()
 
     def on_mount(self) -> None:
--- src/widgets/environment_selector.py
+++ src/widgets/environment_selector.py
@@ -1,13 +1,63 @@
-from typing import override
 from textual.app import ComposeResult
-from textual.widget import Widget
-from textual.widgets import RadioButton, RadioSet
+from textual.containers import Container
+from textual.widgets import Select
 
 
-class EnvrionmentSelector(Widget):
-    @override
+class EnvrionmentSelector(Container):
+    DEFAULT_CSS = """
+    EnvrionmentSelector {
+        width: 1fr;
+        height: 12;
+        padding: 1 2;
+        align: right middle;
+        border: solid orange;
+    }
+
+    EnvrionmentSelector #env-select {
+        width: 1fr;
+        height: 12;
+        border: solid yellow;
+    }
+
+    EnvrionmentSelector #env-select.dev {
+        width: 1fr;
+        height: 12;
+        border: solid purple;
+    }
+
+    EnvrionmentSelector #env-select.np{
+        width:1fr;
+        height: 12;
+        border: solid orange;
+    }
+
+    EnvrionmentSelector #env-select.prod {
+        width: 1fr;
+        height: 12;
+        border: solid pink;
+    }
+
+    """
+
     def compose(self) -> ComposeResult:
-        with RadioSet():
-            yield RadioButton("DEV", value=True)
-            yield RadioButton("NP")
-            yield RadioButton("PROD")
+        yield Select(
+            options=[
+                ("DEV", "dev"),
+                ("NP", "np"),
+                ("PROD", "prod"),
+            ],
+            value="dev",
+            prompt="Chose an enviroment",
+            id="env-select",
+        )
+
+    def on_select_changed(self, event: Select.Changed) -> None:
+        select = event.select
+        select.remove_class("dev", "np", "prod")
+
+        if event.value == "dev":
+            select.add_class("dev")
+        elif event.value == "np":
+            select.add_class("np")
+        elif event.value == "prod":
+            select.add_class("prod")
